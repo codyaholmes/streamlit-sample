d0703387e5d94004f00f72a5e4661a2189397742
cody <codyaholmes@outlook.com>
2026-07-25 21:06:18 -0500

    Add base metric calculations to taxis.py and .gitignore file.

--- pages/taxis.py
+++ pages/taxis.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from data.taxi_trips import load_trips
 
-
 st.title("Taxi Insights")
 
 
@@ -11,8 +10,21 @@ df.columns = [col.title().replace("_", " ") for col in df.columns]
 
 metrics, raw_data = st.tabs(["Metrics", "Raw Data"])
 
+# Metric Calculations
+total_passengers = df["Passengers"].sum()
+total_fare = df["Fare"].sum()
+total_distance = df["Distance"].sum()
+total_tips = df["Tip"].sum()
+
 with metrics:
-    st.text("Metrics go here")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric("Total Passengers", f"{total_passengers:,.0f}", border=True)
+    with col2:
+        st.metric("Total Fares", f"${total_fare:,.0f}", border=True)
 
 with raw_data:
-    st.dataframe(df, hide_index=True)
+    st.dataframe(
+        df,
+    )
